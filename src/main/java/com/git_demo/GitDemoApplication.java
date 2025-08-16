29eebb731d69f9a4a502ca0a7bd2fd2c2398dc52
Taushif Alam <taushifalam70@gmail.com>
2025-08-16 19:39:34 +0530

    Added addCode method

--- src/main/java/com/git_demo/GitDemoApplication.java
+++ src/main/java/com/git_demo/GitDemoApplication.java
@@ -16,6 +16,11 @@ public class GitDemoApplication {
     public String getMessage(@PathVariable  String name) {
         return "Welcome to git tutorial " + name+"It will be fun";
     }
+
+    public void addCode() {
+        System.out.println("This is a new method added to the code");
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
 	}

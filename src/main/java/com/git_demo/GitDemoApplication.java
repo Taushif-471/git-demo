829cdb2d158aa5486d68aa1dfffeee4d5ff0a212
Taushif Alam <taushifalam70@gmail.com>
2025-08-16 18:46:43 +0530

    added project files to staged/local repo

---
+++ src/main/java/com/git_demo/GitDemoApplication.java
@@ -0,0 +1,23 @@
+package com.git_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api")
+public class GitDemoApplication {
+
+    @GetMapping("/message/{name}")
+    public String getMessage(@PathVariable  String name) {
+        return "Welcome to git tutorial " + name;
+    }
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+	}
+
+}
